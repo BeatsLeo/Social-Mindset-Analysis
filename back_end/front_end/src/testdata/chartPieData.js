dc5acd50f97f1e417dc6d271eda5fb0e734b69b4
rachel <2513205019@qq.com>
2023-02-28 22:53:35 +0800

    前后端合并1.0

---
+++ back_end/front_end/src/testdata/chartPieData.js
@@ -0,0 +1,10 @@
+export default [
+  { value: 60, name: "rose 1" },
+  { value: 30, name: "rose 2" },
+  { value: 32, name: "rose 3" },
+  { value: 30, name: "rose 4" },
+  { value: 28, name: "rose 5" },
+  { value: 26, name: "rose 6" },
+  { value: 15, name: "rose 7" },
+  { value: 12, name: "rose 8" }
+]
